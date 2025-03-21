5812e168b1f4739ec150dbf86b0ef25dfbb16ec7
Malchiram <storeflash04@gmail.com>
2025-03-21 21:54:02 +0700

    change path backends

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import axios, { Axios } from 'axios';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const api = (baseUrl?:string): Axios  => {
   const defaultOptions = {
-    baseURL: 'https://backend.gpdishekinah.online:8081/api/',
+    baseURL: 'https://backend.gpdishekinah.online/api/',
     withCredentials: true, // Tambahkan ini supaya cookie dikirim otomatis
   };
 
